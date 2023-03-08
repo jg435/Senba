8ce47b9f3b73f5c512367ca8d20577d422800bcb
Leslie Dees <leslie.dees@duke.edu>
2023-03-08 14:57:41 -0500

    added full texts

--- models/model_comparison.py
+++ models/model_comparison.py
@@ -6,17 +6,22 @@ import csv
 #EVER
 #DO. NOT. RUN. THIS. FILE. OR. WE'RE. FUCKED.
 
-df = pd.read_csv('spam.csv', encoding = 'ISO-8859-1')[['v1','v2']]
+df = pd.read_csv('models/spam.csv', encoding = 'ISO-8859-1')[['v1','v2']]
 text_messages = list(df[df['v1'] == 'ham']['v2'].reset_index()['v2'])
 sampled_text_messages = list()
 num_text_messages = 100
+total_texts_added = 0
 
-for k in range (num_text_messages):
+index_list = []
+
+while total_texts_added <= 100:
     index = random.randint(0, len(text_messages))
     if text_messages[index] not in sampled_text_messages:
         sampled_text_messages.append(text_messages[index])
+    
+    total_texts_added+=1
 
-with open('text_messages_FAKE.csv', 'w', newline='') as f:
+with open('models/text_messages.csv', 'w', newline='') as f:
   writer = csv.writer(f)
   writer.writerow(['Text Message', 'Manual or Machine', 'Positive', 'Negative', 'Neutral', 'Love', 'Joy', 'Sadness', 'Anger', 'Surprise', 'Fear'])
   for message in sampled_text_messages:
