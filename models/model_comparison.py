3f03f24d63e2080edccd1dd0a2271958b3446dfe
Leslie Dees <leslie.dees@duke.edu>
2023-03-08 14:45:41 -0500

    overwrite location of model_comparison

--- models/model_comparison.py
+++ models/model_comparison.py
@@ -2,6 +2,10 @@ import pandas as pd
 import random
 import csv
 
+#DO NOT RUN THIS
+#EVER
+#DO. NOT. RUN. THIS. FILE. OR. WE'RE. FUCKED.
+
 df = pd.read_csv('spam.csv', encoding = 'ISO-8859-1')[['v1','v2']]
 text_messages = list(df[df['v1'] == 'ham']['v2'].reset_index()['v2'])
 sampled_text_messages = list()
@@ -12,7 +16,7 @@ for k in range (num_text_messages):
     if text_messages[index] not in sampled_text_messages:
         sampled_text_messages.append(text_messages[index])
 
-with open('text_messages.csv', 'w', newline='') as f:
+with open('text_messages_FAKE.csv', 'w', newline='') as f:
   writer = csv.writer(f)
   writer.writerow(['Text Message', 'Manual or Machine', 'Positive', 'Negative', 'Neutral', 'Love', 'Joy', 'Sadness', 'Anger', 'Surprise', 'Fear'])
   for message in sampled_text_messages:
