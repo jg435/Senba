5cc78d5ec189ee722c4361aa625b29d3aa37998b
Naman Parikh <naman.parikh@duke.edu>
2023-03-08 14:37:38 -0500

    created model comparison framework for labelling texts

---
+++ models/model_comparison.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import random
+import csv
+
+df = pd.read_csv('spam.csv', encoding = 'ISO-8859-1')[['v1','v2']]
+text_messages = list(df[df['v1'] == 'ham']['v2'].reset_index()['v2'])
+sampled_text_messages = list()
+num_text_messages = 100
+
+for k in range (num_text_messages):
+    index = random.randint(0, len(text_messages))
+    if text_messages[index] not in sampled_text_messages:
+        sampled_text_messages.append(text_messages[index])
+
+with open('text_messages.csv', 'w', newline='') as f:
+  writer = csv.writer(f)
+  writer.writerow(['Text Message', 'Manual or Machine', 'Positive', 'Negative', 'Neutral', 'Love', 'Joy', 'Sadness', 'Anger', 'Surprise', 'Fear'])
+  for message in sampled_text_messages:
+    row_list = [message, 'Manual']
+    writer.writerow(row_list)
+     
+#print(sampled_text_messages)
+
+#text message, love, joy, sadness, anger, surprise, fear, postive, negative, neutral
+    
+
+
+
