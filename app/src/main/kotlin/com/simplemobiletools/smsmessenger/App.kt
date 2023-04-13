b3c3c1ce71864c1f6f532d941d354b689a1ceff6
Yoo Bin Shin <ys230@duke.edu>
2023-04-13 17:01:17 -0400

    Added chaquopy to call python script from app

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/App.kt
@@ -2,10 +2,15 @@ package com.simplemobiletools.smsmessenger
 
 import android.app.Application
 import com.simplemobiletools.commons.extensions.checkUseEnglish
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (! Python.isStarted()) {
+            Python.start(AndroidPlatform(applicationContext));
+        }
         checkUseEnglish()
     }
 }
